d6e86f6f69f6a5f393b57be46f6e385011ed00e2
Mathias Møller <mathiasrask@gmail.com>
2013-10-02 20:21:34 +0200

    inlines for the Tags

--- mpcd/tags/admin.py
+++ mpcd/tags/admin.py
@@ -1,13 +1,23 @@
 from mptt.admin import MPTTModelAdmin
+from django.contrib import admin
+from tags.models import ProcessAlternativeName, MaterialAlternativeName
 #from tags.models import 
 
 ## admin TAGS
+class AlternativeProcessAdmin(admin.TabularInline):
+    model = ProcessAlternativeName
+
+class AlternativeMaterialAdmin(admin.TabularInline):
+    model = MaterialAlternativeName
+
 class MaterialAdmin(MPTTModelAdmin):
     list_display = ['name', 'alternative_names_',]
     search_fields = ['name', 'material_names__name',]
 
     def queryset(self, request):
         return super(MaterialAdmin, self).queryset(request).prefetch_related('material_names')
+
+    inlines = [AlternativeMaterialAdmin,]
 
 class ProcessAdmin(MPTTModelAdmin):
     list_display = ['name', 'alternative_names_',]
@@ -16,6 +26,8 @@ class ProcessAdmin(MPTTModelAdmin):
     def queryset(self, request):
         return super(ProcessAdmin, self).queryset(request).prefetch_related('process_names')
 
+    inlines = [AlternativeProcessAdmin,]
+
 class GeneralTagAdmin(MPTTModelAdmin):
     list_display = ['name',]
     search_fields = ['name',]
@@ -23,3 +35,4 @@ class GeneralTagAdmin(MPTTModelAdmin):
 class MeasurementEquipmentAdmin(MPTTModelAdmin):
     list_display = ['name',]
     search_fields = ['name',]
+
